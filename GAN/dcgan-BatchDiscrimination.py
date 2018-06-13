9077afb6a000ce5ff73c77bb0898895cb0f3c5d7
markliou <markliou@ms51.url.com.tw>
2018-06-13 11:50:38 +0800

    attenuation mini-batch similarity regulor

--- GAN/dcgan-BatchDiscrimination.py
+++ GAN/dcgan-BatchDiscrimination.py
@@ -124,6 +124,7 @@ def main():
     alpha = 1. # constant for weaking the D
     softdec_c = .05 # soft the one-hot
     mbdl_sample_no = 100 # mini batch discrimination sample number
+    mbdl_lambda = 0.01 # attenuation the mini-batch discrimination penality
     
     ## Import MNIST data
     mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
@@ -151,7 +152,7 @@ def main():
     logits_4D = D(tf.concat([gX, X], axis=0), dw) #label: (fake, real)
     logits_4G = D(gX, dw)
     loss_D = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y, logits=logits_4D))
-    loss_G = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(logits_4G), logits=logits_4G)) + mbdl
+    loss_G = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(logits_4G), logits=logits_4G)) + mbdl_lambda * mbdl
     opt_D = tf.train.AdamOptimizer(1e-4, beta1=0.382).minimize(loss_D, var_list = D_var)
     opt_G = tf.train.AdamOptimizer(1e-4, beta1=0.382).minimize(loss_G, var_list = G_var)
     # opt_D = tf.train.AdamOptimizer(1e-6, beta1=0.618).minimize(loss_D, var_list = D_var)
