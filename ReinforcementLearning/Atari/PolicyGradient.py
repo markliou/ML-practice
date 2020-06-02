f5efbff32dc3a7a7f564b0d85613afea3ac575d7
markliou <yifanliou@gmail.com>
2020-06-02 13:52:55 +0000

    fix the cummulative reward

--- ReinforcementLearning/Atari/PolicyGradient.py
+++ ReinforcementLearning/Atari/PolicyGradient.py
@@ -22,13 +22,13 @@ pass
 def Q(S):
     S = (tf.cast(S,tf.float32)-128)/128.  #(210, 160, 3)
     S = tf.layers.Dropout(.5)(S)
-    conv1 = conv2d(S, stride_no=1) #(105, 80)
+    conv1 = conv2d(S, stride_no=2) #(105, 80)
     #conv1 = tf.layers.Dropout(.5)(conv1)
-    conv2 = conv2d(conv1, stride_no=1) #(53, 40)
+    conv2 = conv2d(conv1, stride_no=2) #(53, 40)
     #conv2 = tf.layers.Dropout(.5)(conv2)
-    conv3 = conv2d(conv2, stride_no=1) #(27, 20)
+    conv3 = conv2d(conv2, stride_no=2) #(27, 20)
     #conv3 = tf.layers.Dropout(.5)(conv3)
-    conv4 = conv2d(conv3, stride_no=1) #(14, 10)
+    conv4 = conv2d(conv3, stride_no=2) #(14, 10)
     #conv4 = tf.layers.Dropout(.5)(conv4)
     conv5 = conv2d(conv4, stride_no=2) #(7, 5)
     #conv5 = tf.layers.Dropout(.5)(conv5)
@@ -49,13 +49,14 @@ pass
 STEP_LIMIT = 1000
 EPISODE = 1000
 EPSILONE = .8
-REWARD_b = .1
+REWARD_b = .0
 REWARD_NORMA = 500 # because the peak reward is close to 500, empiritically
 GAMMA = .9
-DIE_PANELTY = 50
-WARMING_EPI = 10
-BEST_REC = 0
-BEST_STEPS = 1
+ALPHA = .9
+DIE_PANELTY = 0
+WARMING_EPI = 0
+BEST_REC = 0.
+BEST_STEPS = 1.
 STATE_GAMMA = .9
 
 env = gym.make('SpaceInvaders-v0') 
@@ -72,8 +73,8 @@ Act_A = Q(Act_S)
 Command_A = tf.argmax(tf.nn.softmax(Act_A), axis=-1)
 
 # PL = Act_R * -tf.log(tf.reduce_sum(tf.nn.softmax(Act_A) * Actions4Act_oh)+1E-9)
-PL = (Act_R/REWARD_NORMA) * tf.nn.softmax_cross_entropy_with_logits(labels=Actions4Act_oh, logits=Act_A)
-Opt = tf.train.RMSPropOptimizer(learning_rate=1E-5, momentum=.0, centered=True).minimize(PL)
+PL = (Act_R) * tf.nn.softmax_cross_entropy_with_logits(labels=Actions4Act_oh, logits=Act_A)
+Opt = tf.train.RMSPropOptimizer(learning_rate=1E-6, momentum=.9, centered=True).minimize(PL)
 #Opt = tf.train.MomentumOptimizer(learning_rate=1E-6, momentum=.8).minimize(PL)
 
 sess = tf.Session()
@@ -82,16 +83,16 @@ sess.run(tf.global_variables_initializer())
 episode = 0
 while(1):
     episode += 1
-    Rp = 0
+    Rp = 0.
     S = env.reset() #(210, 160, 3)
     GameScore = 0
     Clives = 3
-    Reward_cnt = 0
-    CuReward = 0
+    Reward_cnt = 0.
+    CuReward = 0.
     R_list, S_list = [],[]
     
     steps = 0
-    if (np.random.random() >= EPSILONE/np.clip(episode-WARMING_EPI,1E-9,None)) or (WARMING_EPI < episode):
+    if (np.random.random() >= EPSILONE/np.clip(episode-WARMING_EPI,1E-9,None)) or (WARMING_EPI < episode) :
         Greedy_flag = False 
     else:
         Greedy_flag = True 
@@ -120,25 +121,30 @@ while(1):
         GameScore += R
         S = (Sp * STATE_GAMMA + S) * .5  # keep the previoud state as input would be creating a RNN like condition
         if A in [0]:
-            R += (BEST_REC/BEST_STEPS) * .8 # give the reward for moving. This would be helpful for telling agent to avopod bullet
+            R += REWARD_b * 1.2 # give the reward for moving. This would be helpful for telling agent to avopod bullet
         elif A in [2,3]:
-            R += (BEST_REC/BEST_STEPS) * .1
+            R += REWARD_b * 1.5
         pass
         
-        Reward_cnt += R - Rp # advantage, Q
+        # advantage, Q
+        # Reward_cnt = GAMMA * pow((R - Rp),2)  # advantage, Q
+        Reward_cnt = GAMMA * (Rp - R)
         
-        Rp = R
+        Rp = R 
+        #print(R)
 
         # CuReward = CuReward * GAMMA + R
-        # CuReward = CuReward * GAMMA + (Reward_cnt - REWARD_b)
-        CuReward = CuReward * GAMMA + (Reward_cnt - (BEST_REC/BEST_STEPS))
+        CuReward = ALPHA * CuReward + Reward_cnt
+        # CuReward = CuReward * GAMMA + Reward_cnt
+        # CuReward = CuReward * GAMMA + (Reward_cnt - (BEST_REC/BEST_STEPS))
+        #print(CuReward)
 
         # print('Reward:{}'.format(R)) # the reward will give this action will get how much scores. it's descreted.
         # print('Info:{}'.format(info['ale.lives'])) # info in space invader will give the lives of the current state
 
         if finish_flag or (Clives > info['ale.lives']):
             Clives = info['ale.lives']
-            Rp -= (DIE_PANELTY - GameScore) * Clives
+            Rp -= DIE_PANELTY 
             CuReward += Rp
             # CuReward = np.clip(CuReward, 0, None)
             # print('This episode is finished ...')
@@ -151,11 +157,17 @@ while(1):
                 )
             if finish_flag:
                 if BEST_REC < GameScore:
-                    DIE_PANELTY = GameScore
-                    BEST_REC = GameScore
+                    BEST_REC = GameScore 
                 pass
                 if BEST_STEPS < steps:
-                    BEST_STEPS = steps
+                    BEST_STEPS = steps 
+                pass
+                if REWARD_b < (GameScore/steps) :
+                    REWARD_b = (GameScore/steps)
+                pass 
+                if DIE_PANELTY < CuReward:
+                    DIE_PANELTY = CuReward * .5 
+                    #print(DIE_PANELTY)
                 pass
                 os.system("echo {} >> score_rec.txt".format(GameScore))
                 break
@@ -174,7 +186,7 @@ while(1):
         #print('Action:{}  Loss:{} Epsilon:{} greedy:{} score:{}'.format(A, Loss, EPSILONE/np.clip(episode-WARMING_EPI,1E-9,None), Greedy_flag, GameScore))
 
     pass
-    print("Epi:{}  Score:{}  Loss:{}".format(episode,Reward_cnt,Loss))
+    print("Epi:{}  Score:{}  Loss:{}  Reward:{}".format(episode,GameScore,Loss,CuReward))
 
 
 pass
