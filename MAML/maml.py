fix the computation graph

MAML/maml.py
@@ -2,8 +2,8 @@ import numpy as np
 import tensorflow as tf # use the tf2.4
 import tensorflow_datasets as tfds 
 
-inner_lr = 1E-5
-outter_lr = 1E-5
+inner_lr = 1E-3
+outter_lr = 1E-3
 inner_task_loop_no = 128 # give 30 tasks of 2ways-1shot
 opt_inner = tf.keras.optimizers.RMSprop(inner_lr)
 opt_outter = tf.keras.optimizers.RMSprop(outter_lr)
@@ -22,7 +22,7 @@ def cnn():
     return tf.keras.Model(inputs=Input, outputs=out)
 pass 
 
-(kmnist_tr, kmnist_ts) = tfds.load('mnist', 
+(kmnist_tr, kmnist_ts) = tfds.load('kmnist', 
                                    split=['train','test'],
                                    shuffle_files=True,
                                    as_supervised=True,
@@ -47,24 +47,26 @@ loss_inner = lambda: tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labe
 for step in range(5000):
     # keep the original weights for meta-training
     meta_weights = [tf.Variable(target_weights) for target_weights in cnn_model.trainable_weights]
-    outter_loss = 0
+    
+    def meta_loss():
+        loss_outter = 0
+        for task_count in range(inner_task_loop_no):
+            # fetch the dataset. The first dataset is always be 1. 
+            # So, the labels will be defined as the meta-labels insteadly. 
+            imgs, labs = next(kmnist_tr_iter)
+            
+            # put the meta-weights into the model
+            for model_weight_index in range(len(cnn_model.trainable_weights)):
+                cnn_model.trainable_weights[model_weight_index].assign(meta_weights[model_weight_index])
+            pass
 
-    for task_count in range(inner_task_loop_no):
-        # fetch the dataset. The first dataset is always be 1. 
-        # So, the labels will be defined as the meta-labels insteadly. 
-        imgs, labs = next(kmnist_tr_iter)
-        
-        # put the meta-weights into the model
-        for model_weight_index in range(len(cnn_model.trainable_weights)):
-            cnn_model.trainable_weights[model_weight_index].assign(meta_weights[model_weight_index])
-        pass
-
-        # updateing the weights from meta-weights
-        opt_inner.minimize(loss=loss_inner, var_list=cnn_model.trainable_weights) 
-
-        
-        # calculating the task loss (after the meta-weights updating)
-        outter_loss += loss_inner()
+            # updateing the weights from meta-weights
+            opt_inner.minimize(loss=loss_inner, var_list=cnn_model.trainable_weights) 
+            
+            # calculating the task loss (after the meta-weights updating)
+            loss_outter += loss_inner()
+        pass 
+        return loss_outter/inner_task_loop_no
     pass 
 
     # put the meta-weights into the model
@@ -72,15 +74,8 @@ for step in range(5000):
         cnn_model.trainable_weights[model_weight_index].assign(meta_weights[model_weight_index])
     pass
 
-    # calculating the outter loss
-    loss_outter = lambda: tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=meta_labs, logits=cnn_model(imgs))) - \
-                          tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=meta_labs, logits=cnn_model(imgs))) + \
-                          outter_loss/inner_task_loop_no
-
-    # loss_outter = lambda: outter_loss/inner_task_loop_no
-
     # update the meta-weights
-    opt_outter.minimize(loss=loss_outter, var_list=cnn_model.trainable_weights) 
+    opt_outter.minimize(loss=meta_loss, var_list=cnn_model.trainable_weights) 
 
-    print("step: {}  outter_loss: {}".format(step, loss_outter()))
+    print("step: {}  outter_loss: {}".format(step, meta_loss()))
 pass 