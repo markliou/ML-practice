149a47c28ef625b184f0f880e2f423a4a8e41036
markliou <mark.liu@coretronic.com>
2021-06-29 00:13:47 +0800

    forward and backward flow

--- RealNVP/concept_realnvp.py
+++ RealNVP/concept_realnvp.py
@@ -12,7 +12,7 @@ def affine_coupling_block(x, blocks_logs, blocks_t, blockno, forward = True):
         blocks_t.append([])
     pass
     
-    def base_conv2d(blocks, blockno, layerno, output_channel_no = 256):
+    def base_conv2d(blocks, blockno, layerno, output_channel_no = 64):
         if forward:
             if layerno == 0: # the new block need a list for containing the layers
                 blocks[blockno] = []
@@ -34,19 +34,19 @@ def affine_coupling_block(x, blocks_logs, blocks_t, blockno, forward = True):
     for layer_ind in range(1, layer_no):
         latent_logs = base_conv2d(blocks_logs, blockno, layer_ind)(latent_logs)
     pass
-    latent_logs = base_conv2d(blocks_logs, blockno, layer_ind, 8)(latent_logs)
+    latent_logs = tf.nn.tanh(base_conv2d(blocks_logs, blockno,  layer_no, 8)(latent_logs))
 
     latent_t = base_conv2d(blocks_t, blockno, 0)(x)
     for layer_ind in range(1, layer_no):
         latent_t = base_conv2d(blocks_t, blockno, layer_ind)(latent_t)
     pass
-    latent_t = base_conv2d(blocks_t, blockno, layer_ind, 8)(latent_t)
+    latent_t = base_conv2d(blocks_t, blockno, layer_no, 8)(latent_t)
 
     # log Jacobian determinant 在可逆的狀況下，會是"下三角形"，且在對角線之外的數值應該都是0。
     # 因此只需要把log(s)加起來就行了。 
     # ref1. https://proceedings.neurips.cc/paper/2016/hash/ddeebdeefdb7e7e7a697e1c3e3d8ef54-Abstract.html
     # ref2. https://srbittner.github.io/2019/06/26/normalizing_flows/
-    log_det = tf.reduce_sum(latent_logs)
+    log_det = tf.reduce_sum(latent_logs, [-1])
 
     return [latent_logs, latent_t, log_det]
 pass  
@@ -75,26 +75,48 @@ def RealNVP_forward(flow_block_no = 32):
         el_loss += (distribution.log_prob(latent1) + distribution.log_prob(latent2))
     pass
 
-    out = [latent1, latent2]
-    return [tf.keras.Model(inputs=x, outputs=out), [blocks_logs_1, blocks_t_1, blocks_logs_2, blocks_t_2], (logdet_loss + el_loss)]
+    out = [tf.concat([latent1, latent2], axis=-1), -tf.reduce_mean(logdet_loss + el_loss)]
+    return [tf.keras.Model(inputs=x, outputs=out), [blocks_logs_1, blocks_t_1, blocks_logs_2, blocks_t_2]]
 pass 
 
-def RealNVP_backward(): 
+def RealNVP_backward(flow_blocks): 
+    blocks_logs_1, blocks_t_1, blocks_logs_2, blocks_t_2 = flow_blocks
+    block_no = len(blocks_logs_1)
+    x = tf.keras.Input(shape=[7, 7, 16])
+    x1, x2 = tf.split(x, num_or_size_splits=2, axis=-1)
+
+    logs1, t1, logdet1 = affine_coupling_block(x2, blocks_logs_1, blocks_t_1, -1, forward=False)
+    logs2, t2, logdet2 = affine_coupling_block(x1, blocks_logs_2, blocks_t_2, -1, forward=False)
+    latent1 = x1 * tf.exp(logs1) + t1
+    latent2 = x2 * tf.exp(logs2) + t2
+    for flow_block in [b for b in range(1, block_no)[::-1]]:
+        logs1, t1, logdet1 = affine_coupling_block(latent2, blocks_logs_1, blocks_t_1, flow_block, forward=False)
+        logs2, t2, logdet2 = affine_coupling_block(latent1, blocks_logs_2, blocks_t_2, flow_block, forward=False)
+        latent1 = x1 * tf.exp(logs1) + t1
+        latent2 = x2 * tf.exp(logs2) + t2
     pass
+
+    out = tf.concat([latent1, latent2], axis=-1)
+    return tf.keras.Model(inputs=x, outputs=out)
+pass
 
 def batchSqeeze2DFeatureMap(x):
     # normalization
-    x_x = x
-    x_x /= 128 
-    x_x -= 1
+    x_x = tf.cast(x, dtype=tf.float32)
+    x_x /= 128. 
+    x_x -= 1.
     return flo.sqeeze2DFeatureMap(x_x, steps=4)
+pass
+
+def batchUnsqeeze2DFeatureMap(x):
+    return flo.unsqeeze2DFeatureMap(x, steps=4, channel=1)
 pass
 
 def main(): 
 
     ## basic parameter zone 
     batch_size = 32
-    training_interation_no = 5000 
+    training_iter_steps = 5000 
     learning_rate = 1E-4 
     optimizer = tf.keras.optimizers.RMSprop(learning_rate, clipnorm=1)
 
@@ -106,17 +128,30 @@ def main():
                                         as_supervised=False,
                                         with_info=False,)
 
-    kmnist_tr = kmnist_tr.batch(30).repeat()
+    kmnist_tr = kmnist_tr.batch(batch_size).repeat()
     kmnist_tr_iter = iter(kmnist_tr)
 
     # build the RealNVP
-    model, flow_blocks, loss = RealNVP_forward()
-    print(model.summary())
-    exit()
+    model_forward, flow_blocks = RealNVP_forward()
+    model_backward = RealNVP_backward(flow_blocks)
+    # print(model.summary())
 
-    for training_iter_no in range(training_iter_no):
-        sq_tr = tf.map_fn(fn=batchSqeeze2DFeatureMap, elems=tr)
-        print(sq_tr)
+    for training_iter_step in range(training_iter_steps):
+        tr = next(kmnist_tr_iter)
+        sq_tr = tf.map_fn(fn=batchSqeeze2DFeatureMap, elems=tr['image'], dtype=tf.float32)
+
+        def loss():
+            # print(model(sq_tr))
+            return model_forward(sq_tr)[1]
+        pass 
+
+        optimizer.minimize(loss, model_forward.trainable_variables)
+
+        if training_iter_step % 1 == 0:
+            print("step:{}  loss:{}".format(training_iter_step, loss()))
+            me_tr = tf.map_fn(fn=batchUnsqeeze2DFeatureMap, elems=model_forward(sq_tr)[0], dtype=tf.float32)
+            tf.keras.preprocessing.image.save_img("tr.png", me_tr[0],"channels_last")
+        pass
     pass 
 pass  
 
