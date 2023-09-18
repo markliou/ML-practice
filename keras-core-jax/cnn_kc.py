e6e09c73ee72e8771ea8664ac627fed9bbb63ee4
markliou <yifanliou@markliou.tw>
2023-09-18 11:47:57 +0800

    jax backend workarround

--- keras-core-jax/cnn_kc.py
+++ keras-core-jax/cnn_kc.py
@@ -1,4 +1,5 @@
 import tensorflow as tf 
+import tensorflow_datasets as tfds
 import numpy as np 
 import jax 
 import jax.numpy as jnp
@@ -8,6 +9,11 @@ import os
 os.environ["KERAS_BACKEND"] = "jax"
 
 import keras_core as kc
+
+# limited the gpu memory usage when TF working
+gpus = tf.config.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
 
 def cnn():
@@ -26,7 +32,7 @@ def cnn():
 
 def main():
     bs = 32
-    opt = kc.optimizers.AdamW(global_clipnorm = 1.0)
+    # opt = kc.optimizers.AdamW(global_clipnorm = 1.0)
     
     # learning rate shedule
     total_steps = 5000
@@ -35,15 +41,44 @@ def main():
     initial_learning_rate = 0.0
     warmup_target = 1e-4
     alpha = 2e-6
-    lr = tf.keras.optimizers.schedules.CosineDecay(
+    lRFn = tf.keras.optimizers.schedules.CosineDecay(
             initial_learning_rate = initial_learning_rate,
             decay_steps = decay_steps,
             alpha = alpha,
             warmup_target = warmup_target,
-            warmup_steps = warmup_steps,
+            warmup_steps = warmup_steps
             )
     
+    # creating dataset iterator
+    ds = tfds.load('mnist', split="train", shuffle_files=True)
+    ds = ds.shuffle(1024).batch(32).prefetch(tf.data.AUTOTUNE)
+    dsIter = iter(ds)
+    
+    # call the cnn model
     model = cnn()
+    
+    # jax 作為backend，在keras core會把GPU記憶體損耗光，目前原因未知。
+    # 解汙方法：把需要用到GPU記憶體的操作都先做完，最後再來宣告optimizer
+    opt = kc.optimizers.AdamW(global_clipnorm = 1.0)
+    
+    
+    
+    # training loop
+    for step in range(total_steps):
+        # setting the training env variables
+        # opt.lr = lRFn(step)
+        dsFetcher = next(dsIter)
+        cImg = (tf.cast(dsFetcher['image'], tf.float32) - 128) / 128
+        cLab = dsFetcher['label']
+        print(model(cImg))
+        exit()
+        
+        # def loss():
+        #     # pred = model(cImg)
+        #     print(cImg)
+        #     exit()
+        #     pass
+    
 
 if __name__ == "__main__":
     main()    
