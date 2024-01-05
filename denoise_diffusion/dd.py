888ce8466b6e601b353b12a38c9392cf8a83adcc
markliou <yifanliou@gmail.com>
2024-01-05 15:07:56 +0800

    denoise diffussion

--- denoise_diffusion/dd.py
+++ denoise_diffusion/dd.py
@@ -1,2 +1,19 @@
 import tensorflow as tf 
+import numpy as np
 
+def noise_predictor():
+    # Input shapt: (256,256,3)
+    pass
+
+
+def diffussion_schedule():
+    pass
+
+def main():
+    # position embedding tensor.The same shape of input image (256,256,3)
+    emb = tf.Variable()
+pass
+
+if __name__ == '__main__':
+    main()
+pass
