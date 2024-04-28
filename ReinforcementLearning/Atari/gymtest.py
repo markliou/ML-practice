3a32c08042e7a4a99114befe3e8825db38f3c1c1
markliou <yifanliou@markliou.tw>
2024-04-28 17:51:09 +0800

    update the atati sampler

--- ReinforcementLearning/Atari/gymtest.py
+++ ReinforcementLearning/Atari/gymtest.py
@@ -2,21 +2,27 @@ import gym
 import random
 import numpy as np
 from PIL import Image
+import time
 
 # python -m pip install gym[atari]
 # python -m pip install gym[accept-rom-license]
 
-env = gym.make('SpaceInvaders-v0')
+# this will show how the game is playing
+env = gym.make('SpaceInvaders-v4', render_mode='human')
+# env = gym.make('SpaceInvaders-v0')
 
 observation, info = env.reset()
-img = Image.fromarray(observation, "RGB")
-img.show()
+# img = Image.fromarray(observation, "RGB")
+# img.show()
 
 for _ in range(1000):
     observation, reward, terminated, truncated, info = env.step(
         env.action_space.sample())
 
-    print(f"sample:{env.action_space.sample()} , reward:{reward}")
+    # time.sleep(1)
+
+    print(
+        f"sample:{env.action_space.sample()} , reward:{reward}, live:{info['lives']}")
 
     if terminated or truncated:
         observation, info = env.reset()
